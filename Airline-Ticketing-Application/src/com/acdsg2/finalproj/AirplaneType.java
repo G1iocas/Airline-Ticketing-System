b649d9c3c1a6b9413aa750ed601ca0bcf3bdaefe
atthaleen <atthaleen@gmail.com>
2021-05-01 15:25:09 +0800

    AirplaneType Class Update

--- Airline-Ticketing-Application/src/com/acdsg2/finalproj/AirplaneType.java
+++ Airline-Ticketing-Application/src/com/acdsg2/finalproj/AirplaneType.java
@@ -1,5 +1,15 @@
 package com.acdsg2.finalproj;
 
 public class AirplaneType {
-	//Atthaleen
+	static final int PrivateCapacity = 14, BusinessCapacity = 23, RegularFCapacity = 48;
+	static final int Private = 1;
+	static final int Business = 2;
+	static final int RegularF = 3;
+	static int[] seatNum = new int [PrivateCapacity+BusinessCapacity+RegularFCapacity];
+	static boolean[] seatStatus = new boolean[PrivateCapacity+BusinessCapacity+RegularFCapacity];
+	static int PrivateAvailable = PrivateCapacity;
+	static int BusinessAvailable = BusinessCapacity;
+	static int RegularFAvailable = RegularFCapacity;
+	static int seatType = 0;
+	static int seatNumber = 0;
 }
